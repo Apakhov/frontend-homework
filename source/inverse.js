d9ef2c3e15a4df9dc47676fbec7d7bc75b1785f7
Apakhov <m_apakhov@mail.ru>
2019-02-05 15:41:36 +0300

    changed function body for better look

--- source/inverse.js
+++ source/inverse.js
@@ -3,11 +3,10 @@
 const inverse = (xs, n = 0) => {
     let l = Math.max(0, n);
     let r = xs.length - 1 + Math.min(0, n);
-    let m = (l + r) / 2 - l;
-    for (let i = 0; i < m; i++) {
-        let t = xs[l + i];
-        xs[l + i] = xs[r - i];
-        xs[r - i] = t;
+    for (; l < r; l++, r--) {
+        let t = xs[l];
+        xs[l] = xs[r];
+        xs[r] = t;
     }
     return xs;
 };
