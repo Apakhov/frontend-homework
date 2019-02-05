ae200dcbc57f202aa6e6f8d57f5b2e5e6c336578
Apakhov <m_apakhov@mail.ru>
2019-02-05 15:47:58 +0300

    jsdoc added

--- source/inverse.js
+++ source/inverse.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/** inverses array
+ *  @param {any[]} xs - Array of any type
+ *  @param {!number} n - Number of elements from begin (end if negative) that shoud not be affected
+ */
 const inverse = (xs, n = 0) => {
     let l = Math.max(0, n);
     let r = xs.length - 1 + Math.min(0, n);
