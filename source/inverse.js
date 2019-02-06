f54c0558f1c89eabf4393b3ca08765199e411cbc
Apakhov <m_apakhov@mail.ru>
2019-02-07 00:11:26 +0300

    review code fixes

--- source/inverse.js
+++ source/inverse.js
@@ -1,29 +1,29 @@
 'use strict';
 
 /** inverses array
- *  @param {any[]} xs   Array of any type
- *  @param {!number} n  Number of elements from begin (end if negative) that shoud not be affected
- *  @returns {any[]}    Result array
+ *  @param {any[]}   arr    Array of any type
+ *  @param {!number} shift  Number of elements from begin (end if negative) that shoud not be affected
+ *  @returns {any[]}        Result array
  */
-const inverse = (xs, n = 0) => {
-    let l = Math.max(0, n);
-    let r = xs.length - 1 + Math.min(0, n);
+const inverse = (arr, shift = 0) => {
+    let l = Math.max(0, shift);
+    let r = arr.length - 1 + Math.min(0, shift);
     for (; l < r; l++, r--) {
-        let t = xs[l];
-        xs[l] = xs[r];
-        xs[r] = t;
+        const temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
     }
-    return xs;
+    return arr;
 };
 
 /** inverses array
- *  @param {any[]} xs   Array of any type
- *  @param {!number} n  Number of elements from begin (end if negative) that shoud not be affected
- *  @returns {any[]}    Result array
+ *  @param {any[]}   arr    Array of any type
+ *  @param {!number} shift  Number of elements from begin (end if negative) that shoud not be affected
+ *  @returns {any[]}        Result array
  */
-const inverse2 = (xs, n = 0) =>
+const inverse2 = (arr, shift = 0) =>
     (function (l, r) {
         return this.slice(0, l)
             .concat(this.slice(l, r).reverse())
             .concat(this.slice(r))
-    }).call(xs, Math.max(0, n), xs.length + Math.min(0, n));
+    }).call(arr, Math.max(0, shift), arr.length + Math.min(0, shift));
--- tests/inverse.js
+++ tests/inverse.js
@@ -1,11 +1,7 @@
 'use strict';
 
 const createBigArray = (s, e) => {
-	let xs = [];
-	for(let i = s; i < e; i++){
-		xs.push(i);
-	}
-	return xs
+	return Array.from({length: e - s}, (_, i) => s + i);
 }
 
 QUnit.module('Тестируем функцию inverse', function () {
@@ -14,32 +10,32 @@ QUnit.module('Тестируем функцию inverse', function () {
 	});
 
 	QUnit.test('Функция работает с массивом длины один', function (assert) {
-		assert.deepEqual(inverse([1]), [1]);
-		assert.deepEqual(inverse(['a']), ['a']);
-		assert.deepEqual(inverse([null]), [null]);
-		assert.deepEqual(inverse([false]), [false]);
+		assert.deepEqual(inverse([ 1 ]), [ 1 ]);
+		assert.deepEqual(inverse([ 'a' ]), [ 'a' ]);
+		assert.deepEqual(inverse([ null ]), [ null ]);
+		assert.deepEqual(inverse([ false ]), [ false ]);
 	});
 
 	QUnit.test('Функция работает, если в неё передан только массив', function (assert) {
-		assert.deepEqual(inverse([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1]);
-		assert.deepEqual(inverse(['a', 'b', 'c', 'd', 'e']), ['e', 'd', 'c', 'b', 'a']);
-		assert.deepEqual(inverse([null, false, 0, Infinity, '']), ['', Infinity, 0, false, null]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ]), [ 5, 4, 3, 2, 1 ]);
+		assert.deepEqual(inverse([ 'a', 'b', 'c', 'd', 'e' ]), [ 'e', 'd', 'c', 'b', 'a' ]);
+		assert.deepEqual(inverse([ null, false, 0, Infinity, '' ]), [ '', Infinity, 0, false, null ]);
 	});
 
 	QUnit.test('Функция не переставляет первые элементы массива', function (assert) {
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], 0), [5, 4, 3, 2, 1]);
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], 1), [1, 5, 4, 3, 2]);
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], 2), [1, 2, 5, 4, 3]);
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], 5), [1, 2, 3, 4, 5]);
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], 15), [1, 2, 3, 4, 5]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], 0), [ 5, 4, 3, 2, 1 ]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], 1), [ 1, 5, 4, 3, 2 ]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], 2), [ 1, 2, 5, 4, 3 ]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], 5), [ 1, 2, 3, 4, 5 ]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], 15), [ 1, 2, 3, 4, 5 ]);
 	});
 
 	QUnit.test('Функция не переставляет последние элементы массива', function (assert) {
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], 0), [5, 4, 3, 2, 1]);
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], -1), [4, 3, 2, 1, 5]);
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], -2), [3, 2, 1, 4, 5]);
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], -5), [1, 2, 3, 4, 5]);
-		assert.deepEqual(inverse([1, 2, 3, 4, 5], -15), [1, 2, 3, 4, 5]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], 0), [ 5, 4, 3, 2, 1 ]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], -1), [ 4, 3, 2, 1, 5 ]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], -2), [ 3, 2, 1, 4, 5 ]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], -5), [ 1, 2, 3, 4, 5 ]);
+		assert.deepEqual(inverse([ 1, 2, 3, 4, 5 ], -15), [ 1, 2, 3, 4, 5 ]);
 	});
 
 
@@ -57,32 +53,32 @@ QUnit.module('Тестируем функцию inverse2', function () {
 	});
 
 	QUnit.test('Функция работает с массивом длины один', function (assert) {
-		assert.deepEqual(inverse2([1]), [1]);
-		assert.deepEqual(inverse2(['a']), ['a']);
-		assert.deepEqual(inverse2([null]), [null]);
-		assert.deepEqual(inverse2([false]), [false]);
+		assert.deepEqual(inverse2([ 1 ]), [ 1 ]);
+		assert.deepEqual(inverse2([ 'a' ]), [ 'a' ]);
+		assert.deepEqual(inverse2([ null ]), [ null ]);
+		assert.deepEqual(inverse2([ false ]), [ false ]);
 	});
 
 	QUnit.test('Функция работает, если в неё передан только массив', function (assert) {
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1]);
-		assert.deepEqual(inverse2(['a', 'b', 'c', 'd', 'e']), ['e', 'd', 'c', 'b', 'a']);
-		assert.deepEqual(inverse2([null, false, 0, Infinity, '']), ['', Infinity, 0, false, null]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ]), [ 5, 4, 3, 2, 1 ]);
+		assert.deepEqual(inverse2([ 'a', 'b', 'c', 'd', 'e' ]), [ 'e', 'd', 'c', 'b', 'a' ]);
+		assert.deepEqual(inverse2([ null, false, 0, Infinity, '' ]), [ '', Infinity, 0, false, null ]);
 	});
 
 	QUnit.test('Функция не переставляет первые элементы массива', function (assert) {
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 0), [5, 4, 3, 2, 1]);
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 1), [1, 5, 4, 3, 2]);
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 2), [1, 2, 5, 4, 3]);
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 5), [1, 2, 3, 4, 5]);
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 15), [1, 2, 3, 4, 5]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], 0), [ 5, 4, 3, 2, 1 ]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], 1), [ 1, 5, 4, 3, 2 ]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], 2), [ 1, 2, 5, 4, 3 ]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], 5), [ 1, 2, 3, 4, 5 ]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], 15), [ 1, 2, 3, 4, 5 ]);
 	});
 
 	QUnit.test('Функция не переставляет последние элементы массива', function (assert) {
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 0), [5, 4, 3, 2, 1]);
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], -1), [4, 3, 2, 1, 5]);
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], -2), [3, 2, 1, 4, 5]);
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], -5), [1, 2, 3, 4, 5]);
-		assert.deepEqual(inverse2([1, 2, 3, 4, 5], -15), [1, 2, 3, 4, 5]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], 0), [ 5, 4, 3, 2, 1 ]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], -1), [ 4, 3, 2, 1, 5 ]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], -2), [ 3, 2, 1, 4, 5 ]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], -5), [ 1, 2, 3, 4, 5 ]);
+		assert.deepEqual(inverse2([ 1, 2, 3, 4, 5 ], -15), [ 1, 2, 3, 4, 5 ]);
 	});
 
 
