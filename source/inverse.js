0770bf0f0e05f0f6b75f45ad93759e9b2b534d29
Apakhov <m_apakhov@mail.ru>
2019-02-06 16:26:42 +0300

    inverse (inverse2) using reverse

--- source/inverse.js
+++ source/inverse.js
@@ -1,8 +1,9 @@
 'use strict';
 
 /** inverses array
- *  @param {any[]} xs - Array of any type
- *  @param {!number} n - Number of elements from begin (end if negative) that shoud not be affected
+ *  @param {any[]} xs   Array of any type
+ *  @param {!number} n  Number of elements from begin (end if negative) that shoud not be affected
+ *  @returns {any[]}    Result array
  */
 const inverse = (xs, n = 0) => {
     let l = Math.max(0, n);
@@ -14,3 +15,15 @@ const inverse = (xs, n = 0) => {
     }
     return xs;
 };
+
+/** inverses array
+ *  @param {any[]} xs   Array of any type
+ *  @param {!number} n  Number of elements from begin (end if negative) that shoud not be affected
+ *  @returns {any[]}    Result array
+ */
+const inverse2 = (xs, n = 0) =>
+    (function (l, r) {
+        return this.slice(0, l)
+            .concat(this.slice(l, r).reverse())
+            .concat(this.slice(r))
+    }).call(xs, Math.max(0, n), xs.length + Math.min(0, n));
--- tests/inverse.js
+++ tests/inverse.js
@@ -50,3 +50,46 @@ QUnit.module('Тестируем функцию inverse', function () {
 		assert.deepEqual(inverse(createBigArray(0, 1000000), -50000), createBigArray(0, 1000000 - 50000).reverse().concat(createBigArray(1000000 - 50000, 1000000)))
 	})
 });
+
+QUnit.module('Тестируем функцию inverse2', function () {
+	QUnit.test('Функция работает с пустым массивом', function (assert) {
+		assert.deepEqual(inverse2([]), []);
+	});
+
+	QUnit.test('Функция работает с массивом длины один', function (assert) {
+		assert.deepEqual(inverse2([1]), [1]);
+		assert.deepEqual(inverse2(['a']), ['a']);
+		assert.deepEqual(inverse2([null]), [null]);
+		assert.deepEqual(inverse2([false]), [false]);
+	});
+
+	QUnit.test('Функция работает, если в неё передан только массив', function (assert) {
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1]);
+		assert.deepEqual(inverse2(['a', 'b', 'c', 'd', 'e']), ['e', 'd', 'c', 'b', 'a']);
+		assert.deepEqual(inverse2([null, false, 0, Infinity, '']), ['', Infinity, 0, false, null]);
+	});
+
+	QUnit.test('Функция не переставляет первые элементы массива', function (assert) {
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 0), [5, 4, 3, 2, 1]);
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 1), [1, 5, 4, 3, 2]);
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 2), [1, 2, 5, 4, 3]);
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 5), [1, 2, 3, 4, 5]);
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 15), [1, 2, 3, 4, 5]);
+	});
+
+	QUnit.test('Функция не переставляет последние элементы массива', function (assert) {
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], 0), [5, 4, 3, 2, 1]);
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], -1), [4, 3, 2, 1, 5]);
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], -2), [3, 2, 1, 4, 5]);
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], -5), [1, 2, 3, 4, 5]);
+		assert.deepEqual(inverse2([1, 2, 3, 4, 5], -15), [1, 2, 3, 4, 5]);
+	});
+
+
+
+	QUnit.test('Функция работает на больших данных', function (assert) {
+		assert.deepEqual(inverse2(createBigArray(0, 1000000)), createBigArray(0, 1000000).reverse())
+		assert.deepEqual(inverse2(createBigArray(0, 1000000), 50000), createBigArray(0, 50000).concat(createBigArray(50000, 1000000).reverse()))
+		assert.deepEqual(inverse2(createBigArray(0, 1000000), -50000), createBigArray(0, 1000000 - 50000).reverse().concat(createBigArray(1000000 - 50000, 1000000)))
+	})
+});
